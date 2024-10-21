b8114207677ef33124f4c05c39ecff9518a1d678
SveOls <SveOls@github.com>
2024-10-21 07:48:43 +0200

    added fortls

--- init.lua
+++ init.lua
@@ -609,6 +609,8 @@ require('lazy').setup({
         -- gopls = {},
         -- pyright = {},
         rust_analyzer = {},
+        -- had to run "sudo apt install python3.10-venv" or mason wouldnt install this
+        fortls = {},
         -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
         --
         -- Some languages (like typescript) have entire language plugins that can be useful:
--- lua/custom/plugins/harpoon.lua
+++ lua/custom/plugins/harpoon.lua
@@ -4,26 +4,26 @@ return {
     local mark = require 'harpoon.mark'
     local ui = require 'harpoon.ui'
 
-    vim.keymap.set('n', '<leader>ha', mark.add_file, { desc = 'add file to [H]arpoon' })
-    vim.keymap.set('n', '<leader>hm', ui.toggle_quick_menu, { desc = 'toggle quick[M]enu' })
+    vim.keymap.set('n', '<leader>ja', mark.add_file, { desc = '[A]dd file to harpoon' })
+    vim.keymap.set('n', '<leader>jm', ui.toggle_quick_menu, { desc = 'toggle quick[M]enu' })
 
-    vim.keymap.set('n', '<leader>h1', function()
+    vim.keymap.set('n', '<leader>j1', function()
       ui.nav_file(1)
     end, { desc = 'Go to first mark' })
 
-    vim.keymap.set('n', '<leader>h2', function()
+    vim.keymap.set('n', '<leader>j2', function()
       ui.nav_file(2)
     end, { desc = 'Go to second mark' })
 
-    vim.keymap.set('n', '<leader>h3', function()
+    vim.keymap.set('n', '<leader>j3', function()
       ui.nav_file(3)
     end, { desc = 'Go to third mark' })
 
-    vim.keymap.set('n', '<leader>h4', function()
+    vim.keymap.set('n', '<leader>j4', function()
       ui.nav_file(4)
     end, { desc = 'Go to fourth mark' })
 
-    vim.keymap.set('n', '<leader>hg', function()
+    vim.keymap.set('n', '<leader>jg', function()
       ui.nav_file(vim.v.count)
     end, { desc = '[G]o to {vim.v.count} mark' })
   end,
