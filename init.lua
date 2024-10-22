98d8d76d014b71675217f43afe8e77a7c0f1e8de
SveOls <sverre.d.olsen@live.no>
2024-10-22 12:47:21 +0200

    update

--- init.lua
+++ init.lua
@@ -610,7 +610,7 @@ require('lazy').setup({
         -- pyright = {},
         rust_analyzer = {},
         -- had to run "sudo apt install python3.10-venv" or mason wouldnt install this
-        fortls = {},
+        -- fortls = {},
         -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
         --
         -- Some languages (like typescript) have entire language plugins that can be useful:
