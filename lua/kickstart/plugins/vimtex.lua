bd4f99fab70a9830e2035588f5e9ed54e5d0b569
SveOls <SveOls@github.com>
2025-01-02 06:31:39 +0100

    oh

--- lua/kickstart/plugins/vimtex.lua
+++ lua/kickstart/plugins/vimtex.lua
@@ -2,6 +2,9 @@ return {
   'lervag/vimtex',
   lazy = false,
   init = function()
-    vim.g.vimtex_view_method = 'zathura'
+    -- vim.g.vimtex_view_method = 'general'
+    vim.g.vimtex_view_general_viewer = 'okular'
+    vim.g.viewex_view_general_options = '--unique file:@pdf#src:@line@tex'
+    vim.g.vimtex_quickfix_mode = 0
   end,
 }
---
+++ lua/kickstart/plugins/yuck.lua
@@ -0,0 +1,4 @@
+return {
+  'elkowar/eww',
+  lazy = false,
+}
