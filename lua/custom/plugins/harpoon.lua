5ffe74f961387f77bffe2d1281e3c023511215bf
SveOls <SveOls@github.com>
2025-01-31 17:12:55 +0000

    oh

--- lua/custom/plugins/harpoon.lua
+++ lua/custom/plugins/harpoon.lua
@@ -23,6 +23,26 @@ return {
       ui.nav_file(4)
     end, { desc = 'Go to fourth mark' })
 
+    vim.keymap.set('n', '<leader>j5', function()
+      ui.nav_file(5)
+    end, { desc = 'Go to fifth mark' })
+
+    vim.keymap.set('n', '<leader>j6', function()
+      ui.nav_file(6)
+    end, { desc = 'Go to sixth mark' })
+
+    vim.keymap.set('n', '<leader>j7', function()
+      ui.nav_file(7)
+    end, { desc = 'Go to seventh mark' })
+
+    vim.keymap.set('n', '<leader>j8', function()
+      ui.nav_file(8)
+    end, { desc = 'Go to eighth mark' })
+
+    vim.keymap.set('n', '<leader>j9', function()
+      ui.nav_file(9)
+    end, { desc = 'Go to ninth mark' })
+
     vim.keymap.set('n', '<leader>jg', function()
       ui.nav_file(vim.v.count)
     end, { desc = '[G]o to {vim.v.count} mark' })
--- lua/kickstart/plugins/vimtex.lua
+++ lua/kickstart/plugins/vimtex.lua
@@ -5,6 +5,7 @@ return {
     -- vim.g.vimtex_view_method = 'general'
     vim.g.vimtex_view_general_viewer = 'okular'
     vim.g.viewex_view_general_options = '--unique file:@pdf#src:@line@tex'
-    vim.g.vimtex_quickfix_mode = 0
+    -- vim.g.vimtex_quickfix_mode = 0
+    --
   end,
 }
